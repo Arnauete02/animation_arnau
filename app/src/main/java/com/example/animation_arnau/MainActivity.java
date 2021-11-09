e33a028f3763f4a75e85df7e3d9f6df485c51bc1
Arnauete02 <agraciataberner02@gmail.com>
2021-11-09 10:18:10 +0100

    runnable & handler

--- app/src/main/java/com/example/animation_arnau/MainActivity.java
+++ app/src/main/java/com/example/animation_arnau/MainActivity.java
@@ -2,15 +2,23 @@ package com.example.animation_arnau;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActivityOptions;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.util.Pair;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    ImageView logo;
-    TextView title1;
-    TextView subtitle1;
+    public static int SPLASH_SCREEN = 2;
+
+    private ImageView logo;
+    private TextView title1;
+    private TextView subtitle1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +28,21 @@ public class MainActivity extends AppCompatActivity {
         logo = findViewById(R.id.logo);
         title1 = findViewById(R.id.title1);
         subtitle1 = findViewById(R.id.subtitle1);
+
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                //Intent
+                Intent intent = new Intent(MainActivity.this, DashboardActivity.class);
+                Pair[] pairs = new Pair[2];
+                pairs[0] = new Pair<View,String>(logo, "logo_origen");
+                ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(MainActivity.this,
+                        pairs);
+                startActivity(intent, options.toBundle());
+            }
+        };
+
+        Handler handler = new Handler(Looper.getMainLooper());
+        handler.postDelayed(runnable,SPLASH_SCREEN);
     }
 }
